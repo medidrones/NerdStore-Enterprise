21e657a4d7f16eb6a9d4c9ed1d879e80860a04ce
Jorge Medina <medidrones@gmail.com>
2025-11-13 21:05:22 -0300

    Feat: Implementado Projeto Ecommerce Nerd Store

---
+++ src/web/NSE.WebApp.MVC/Models/PedidoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class PedidoViewModel
+    {
+        #region Pedido
+
+        public int Codigo { get; set; }       
+        public int Status { get; set; }
+        public DateTime Data { get; set; }
+        public decimal ValorTotal { get; set; }
+
+        public decimal Desconto { get; set; }
+        public bool VoucherUtilizado { get; set; }
+
+        public List<ItemPedidoViewModel> PedidoItems { get; set; } = new List<ItemPedidoViewModel>();
+
+        #endregion
+
+        #region Item Pedido
+
+        public class ItemPedidoViewModel
+        {
+            public Guid ProdutoId { get; set; }
+            public string Nome { get; set; }
+            public int Quantidade { get; set; }
+            public decimal Valor { get; set; }
+            public string Imagem { get; set; }
+        }
+
+        #endregion
+
+        #region Endereco
+
+        public EnderecoViewModel Endereco { get; set; }
+
+        #endregion
+    }
+}
